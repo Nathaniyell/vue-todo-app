efd8e3c957c861c73debf57c86ff61503f7246b4
Nathaniyell <essien.nathan@yahoo.com>
2024-09-13 00:15:54 +0100

    update application header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,7 +7,7 @@ defineProps({
 
 <template>
     <div>
-      <h1 class="text-center p-4 mb-3">Todo Test Application</h1>
+      <h1 class="text-center p-4 mb-3">Vue Taskify</h1>
       <div class="form-label hstack gap-3 mb-4 h6 text-primary">
         <span>Total {{ tasks.length > 1 ? 'tasks' : 'task' }}: {{ tasks.length }}</span>
         <span class="ms-auto text-success">Completed Task: {{ completedTasks }}</span>
